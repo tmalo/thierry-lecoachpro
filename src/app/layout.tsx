bf666c376f535087bc107c34ecda2f4171a185d7
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2025-06-19 18:40:42 +0200

    umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
-import type { Metadata } from "next";
-import { Montserrat, Merriweather, Cormorant_Garamond } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Montserrat, Merriweather, Cormorant_Garamond } from 'next/font/google'
+import './globals.css'
+import { config } from '@/lib/config'
 
 const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-  display: "swap",
-});
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const merriweather = Merriweather({
-  variable: "--font-merriweather",
-  subsets: ["latin"],
-  weight: ["400", "700", "900"],
-  display: "swap"
-});
+  variable: '--font-merriweather',
+  subsets: ['latin'],
+  weight: ['400', '700', '900'],
+  display: 'swap',
+})
 
 const cormorant = Cormorant_Garamond({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-cormorant",
-  display: "swap",
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-cormorant',
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -30,15 +31,22 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <script
+          defer
+          src={config.umami.src}
+          data-website-id={config.umami.websiteId}
+        ></script>
+      </head>
       <body
         className={`${montserrat.variable} ${merriweather.variable} ${cormorant.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
