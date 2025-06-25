0377378748aa56d507f9c70066e554b3834b7960
Thierry Malo <thierry.malo@ringover.com>
2025-06-25 15:13:05 +0200

    section component

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,27 +1,43 @@
-import { tv } from 'tailwind-variants';
+import { tv } from "tailwind-variants";
 
 const sx = tv({
-	base: 'py-20',
-	variants: {
-		style: {
-			gradient: 'bg-gradient-to-br from-white to-gray-50',
-			gray: 'bg-gray-50',
-			primary: 'bg-primary text-white',
-			white: 'bg-white',
-			none: '',
-		},
-	},
-	defaultVariants: {
-		style: 'none',
-	}
-})
+  base: "py-20",
+  variants: {
+    style: {
+      gradient: "bg-gradient-to-br from-white to-gray-50",
+      gray: "bg-gray-50",
+      primary: "bg-primary text-white",
+      white: "bg-white",
+      none: "",
+    },
+  },
+  defaultVariants: {
+    style: "none",
+  },
+});
 
-export default function Section({ children, style = 'none', centered = false }: any) {
-	return (
-		<section className={sx({ style })}>
-			<div className={centered ? 'container-max section-padding text-center' : 'container-max section-padding'}>
-				{children}
-			</div>
-		</section>
-	)
+type SectionProps = Readonly<{
+  children: React.ReactNode;
+  style: "none" | "gradient" | "gray" | "primary" | "white" | undefined;
+  centered: boolean;
+}>;
+
+export default function Section({
+  children,
+  style = "none",
+  centered = false,
+}: SectionProps) {
+  return (
+    <section className={sx({ style })}>
+      <div
+        className={
+          centered
+            ? "container-max section-padding text-center"
+            : "container-max section-padding"
+        }
+      >
+        {children}
+      </div>
+    </section>
+  );
 }
