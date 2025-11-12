8da6fc398c4edbd105ff55b15e5de6cbef737cee
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2025-11-12 13:11:37 +0100

    visibility correction

--- src/app/coaching-approche-sensible-alignement/_components/section-detail.tsx
+++ src/app/coaching-approche-sensible-alignement/_components/section-detail.tsx
@@ -12,10 +12,10 @@ export function SectionDefail({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto grid max-w-6xl grid-cols-5 grid-rows-1 gap-2">
-      <div className=" rounded-2xl bg-gradient-to-br p-4  ">
+    <div className="mx-auto md:grid max-w-6xl md:grid-cols-5 grid-rows-1 md:gap-2">
+      <div className=" rounded-2xl bg-gradient-to-br p-4 invisible md:visible  ">
         <Image
-          className="h-60 w-34 m-auto rounded-lg object-scale-down shadow-lg"
+          className="h-0 w-0 md:h-60 md:w-34 m-auto rounded-lg object-scale-down invisible md:visible  shadow-lg"
           src={img}
           alt={alt}
           width={2268}
@@ -23,7 +23,7 @@ export function SectionDefail({
           objectFit="contain"
         />
       </div>
-      <div className="col-span-4">
+      <div className=" md:col-span-4">
         <h2 className="font-montserrat text-primary mb-8 text-3xl font-bold">
           {title}
         </h2>
